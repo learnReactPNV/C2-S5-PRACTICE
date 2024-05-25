3cf03b8e507426a8309966ac19900f2b47043311
Huyen Ho <hothihuyen06031195@gmail.com>
2024-05-25 09:56:21 +0700

    Done exercise 4

--- exercise-4/src/App.jsx
+++ exercise-4/src/App.jsx
@@ -12,6 +12,9 @@ function App() {
         <section className="places-category">
           <ul className="places">
             {/* For each place from AVAILABLE_PLACES, create a Place component */}
+            {AVAILABLE_PLACES.map((place, index) => (
+              <Place key={index} place={place} />
+            ))}
           </ul>
         </section>
       </main>
